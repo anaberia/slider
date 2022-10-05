0e134113fce1e268537fd0c114c878fd93951ec4
anaberia <ana.beria3@gmail.com>
2022-10-04 21:25:08 -0700

    add states and span

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 
 function App() {
-  return <div className="App"></div>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+    </section>
+  );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,36 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote: "text",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "john doe",
+    title: "regular guy",
+    quote: "text",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    name: "peter smith",
+    title: "product designer",
+    quote: "text",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "susan andersen",
+    title: "the boss",
+    quote: "text",
+  },
+];
+
+export default people;
