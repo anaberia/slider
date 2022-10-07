536ca1825da24ecf16988c957e92cf2441c5f450
anaberia <ana.beria3@gmail.com>
2022-10-06 18:07:11 -0700

    add slider functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
   return (
     <section className="section">
       <div className="title">
@@ -16,20 +17,30 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
           return (
-            <div key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
               <p className="text">{quote}</p>
               <FaQuoteRight className="icon" />
-            </div>
+            </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
